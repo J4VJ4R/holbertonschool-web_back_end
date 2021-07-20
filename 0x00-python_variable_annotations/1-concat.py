1ceba6b069fcefa79cd9bfd434438ccb1dcbe4a2
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-07-20 08:32:18 -0500

    adding new correction

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-"""Basic annotations task 1 """
+"""concat module
+"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """Concatenates two strings
+    """concat two strings
+
     Args:
-        str1: string
-        str2: string
-    Return: concatenated string
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        str: str1 + str2
     """
-    return "{}{}".format(str1, str2)
+    return str1 + str2
 {"mode":"full","isActive":false}
